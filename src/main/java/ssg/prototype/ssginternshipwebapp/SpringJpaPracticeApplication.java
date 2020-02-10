dffc8e581eb4b5c127a3af8fea4a88a13fbc3549
Jungyun Oh <ohdev12@naver.com>
2020-02-10 19:30:00 +0900

    Revert "github test comment"
    This reverts commit e152541fafe52fd4134518a2f4ff23dc18b1832c.

--- src/main/java/ssg/prototype/ssginternshipwebapp/SpringJpaPracticeApplication.java
+++ src/main/java/ssg/prototype/ssginternshipwebapp/SpringJpaPracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJpaPracticeApplication {
-	// github test comment
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaPracticeApplication.class, args);
 	}
