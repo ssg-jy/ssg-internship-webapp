b1b05e0e8da730b1febdbd6fae5e22e107dcaa8f
오정윤/신입/q80037 <liz122121@gmail.com>
2020-02-14 19:28:48 +0900

    sql 상품 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
 INSERT INTO CUSTOMER VALUES (1, 'test', 'test'),(2, 'test2', 'test2');
 
-INSERT INTO PRODUCT VALUES (1, '포카칩', 2000, 5)
-,(2, '스윙칩', 2000, 5)
-,(3, '칩포테토', 1500, 5);
+INSERT INTO PRODUCT VALUES (1, '포카칩', 2000, 50)
+,(2, '스윙칩', 2000, 50)
+,(3, '칩포테토', 1500, 50)
+,(4, '눈을감자', 1500, 50)
+,(5, '구운감자', 1500, 50)
+,(6, '예감', 1500, 50)
+,(7, '오감자', 1500, 50);
 
 INSERT INTO VARIABLE VALUES ('ocount', 1);
