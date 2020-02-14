91a05baa8f2557a4f9c267d7d694c76a21f9b03e
오정윤/신입/q80037 <liz122121@gmail.com>
2020-02-14 18:07:13 +0900

    주문코드, 주문상태 나타내는 상수들 정의(임시, enum으로 바꿔야!)

---
+++ src/main/java/ssg/prototype/ssginternshipwebapp/OrdCode.java
@@ -0,0 +1,9 @@
+package ssg.prototype.ssginternshipwebapp;
+
+public class OrdCode {
+	public static final int ORDER = 1;
+	public static final int ADD_ORD = 2;
+	public static final int PART_CANCEL = 3;
+	public static final int ALL_CANCEL = 4;
+	
+}
---
+++ src/main/java/ssg/prototype/ssginternshipwebapp/OrdStat.java
@@ -0,0 +1,8 @@
+package ssg.prototype.ssginternshipwebapp;
+
+public class OrdStat {
+	public static final int ORD_COMPLETE = 1;
+	public static final int DLV_ING = 2;
+	public static final int DLV_COMPLETE = 3;
+	
+}
