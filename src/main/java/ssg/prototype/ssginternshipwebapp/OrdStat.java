7326b0ba92f24ba7b6c4241173aca4c99db63812
오정윤/신입/q80037 <liz122121@gmail.com>
2020-02-17 20:24:02 +0900

    주문상태 관련 상수 정리

--- src/main/java/ssg/prototype/ssginternshipwebapp/OrdStat.java
+++ src/main/java/ssg/prototype/ssginternshipwebapp/OrdStat.java
@@ -1,8 +1,10 @@
 package ssg.prototype.ssginternshipwebapp;
 
 public class OrdStat {
-	public static final int ORD_COMPLETE = 1;
-	public static final int DLV_ING = 2;
-	public static final int DLV_COMPLETE = 3;
+	public static final int ORD_COMPLETE = 0;
+	public static final int DLV_ING = 1;
+	public static final int DLV_COMPLETE = 2;
 	
+	public static final String[] stat_string = {"주문완료", "배송중", "배송완료"};
+	public static final int[] stat_int = {ORD_COMPLETE, DLV_ING, DLV_COMPLETE};
 }
