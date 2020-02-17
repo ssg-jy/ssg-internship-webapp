8d1c864d2b932d975a159ff8829b9f721007d303
오정윤/신입/q80037 <liz122121@gmail.com>
2020-02-17 20:24:51 +0900

    주문상세보기에서 주문상태 정보 추가

--- src/main/java/ssg/prototype/ssginternshipwebapp/controller/OrderController.java
+++ src/main/java/ssg/prototype/ssginternshipwebapp/controller/OrderController.java
@@ -1,5 +1,6 @@
 package ssg.prototype.ssginternshipwebapp.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ssg.prototype.ssginternshipwebapp.OrdStat;
+import ssg.prototype.ssginternshipwebapp.Status;
 import ssg.prototype.ssginternshipwebapp.domain.entity.Customer;
 import ssg.prototype.ssginternshipwebapp.domain.entity.Jumun;
 import ssg.prototype.ssginternshipwebapp.domain.entity.JumunDetail;
@@ -71,9 +74,17 @@ public class OrderController {
 		* 여기서 oid로 db 조회해서 사용자 아이디와 session의 cid 가 동일한지 확인해야한다!!
 		*/
 		List<Jumun> ord = orderRepository.findByOrderId(oid);
-		model.addAttribute("orderDate", ord.get(0).getOrderedDate());
+		List<Status> statuses = new ArrayList<Status>();
+		statuses.add(new Status(OrdStat.stat_string[0], false));
+		statuses.add(new Status(OrdStat.stat_string[1], false));
+		statuses.add(new Status(OrdStat.stat_string[2], false));
+		statuses.get(ord.get(0).getStatus()).setNow(true);
+		
 		List<JumunDetail> orderDetails = orderDetailService.showOrder(oid);
 		List<Product> products = productService.findProductsById(orderDetails);
+		
+		model.addAttribute("orderDate", ord.get(0).getOrderedDate());
+		model.addAttribute("statuses", statuses);
 		model.addAttribute("orderDetails", orderDetails);
 		model.addAttribute("products", products);
 		model.addAttribute("name", session.getAttribute("cname"));
